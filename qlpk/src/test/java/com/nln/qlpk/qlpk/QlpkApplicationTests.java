7d2af6b09176b17eb24a74682569eb60c994daf6
halleon2002 <82798557+halleon2002@users.noreply.github.com>
2024-04-18 17:28:42 +0700

    directory qlpk

---
+++ qlpk/src/test/java/com/nln/qlpk/qlpk/QlpkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nln.qlpk.qlpk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlpkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
