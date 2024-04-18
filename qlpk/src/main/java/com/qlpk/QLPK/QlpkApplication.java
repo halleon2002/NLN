77fbfdb6116dd9cabcfaa4560fb0c01c36f0ab80
halleon2002 <82798557+halleon2002@users.noreply.github.com>
2024-04-18 17:47:29 +0700

    test

---
+++ qlpk/src/main/java/com/qlpk/QLPK/QlpkApplication.java
@@ -0,0 +1,13 @@
+package com.qlpk.QLPK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlpkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlpkApplication.class, args);
+	}
+
+}
